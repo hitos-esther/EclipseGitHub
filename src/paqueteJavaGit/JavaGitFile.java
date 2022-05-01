05498f2e5728dc0f08f320284c4147b2952e9385
estherhitos <esther.hitos@iescampanillas.com>
2022-05-01 09:05:07 +0200

    1-Primer commit con variable Nombre y salida de pantalla con variable nombre

---
+++ src/paqueteJavaGit/JavaGitFile.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package paqueteJavaGit;
+
+/**
+ * @author ESTHER
+ *
+ */
+public class JavaGitFile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
