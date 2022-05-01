d82417cda6403136c33fd4094b17bb914ad4a6cb
estherhitos <esther.hitos@iescampanillas.com>
2022-05-01 09:14:17 +0200

    2-Segundo commit con variable apellido y salida por pantalla con variable apellido

--- src/paqueteJavaGit/JavaGitFile.java
+++ src/paqueteJavaGit/JavaGitFile.java
@@ -8,13 +8,16 @@ package paqueteJavaGit;
  *
  */
 public class JavaGitFile {
-
+	static String nombre="Esther";
+	static String apellido="Hitos";
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Mi nombre es:"+nombre);
+		System.out.println("El primer Apellido es: "+apellido);
 	}
 
 }
